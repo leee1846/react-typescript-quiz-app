ae52b317ac27e3581172e0ee1edbe36551377bc1
leee1846 <leee1846@gmail.com>
2021-02-16 12:27:05 +0900

    feature: questioncard 아웃풋 구성

--- src/components/questionCard/QuestionCard.tsx
+++ src/components/questionCard/QuestionCard.tsx
@@ -1,9 +1,37 @@
 import React from "react";
 
-const QuestionCard = () => {
+type Props = {
+  question: string;
+  answers: string[];
+  callback: any;
+  userAnswer: boolean;
+  questionNumber: number;
+  totalQuestions: number;
+};
+
+const QuestionCard: React.FC<Props> = ({
+  question,
+  answers,
+  callback,
+  userAnswer,
+  questionNumber,
+  totalQuestions,
+}) => {
   return (
     <div>
-      <h1>s</h1>
+      <p>
+        Question : {questionNumber} / {totalQuestions}
+      </p>
+      <p dangerouslySetInnerHTML={{ __html: question }}></p>
+      <div>
+        {answers.map((answer) => (
+          <div>
+            <button disabled={userAnswer} onClick={callback}>
+              <span dangerouslySetInnerHTML={{ __html: answer }}></span>
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
