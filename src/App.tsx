27c0bd259d6b25cefca79504a21b93b872e9b368
leee1846 <leee1846@gmail.com>
2021-02-16 13:33:28 +0900

    feature: api체크

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import { useState } from "react";
 import QuestionCard from "./components/questionCard/QuestionCard";
+import { fetchQuizQuestions } from "./api/API";
+//TYPES
+import { Difficulty } from "./api/API";
+
+const TOTAL_QUESTIONS = 10;
 
 function App() {
   const [loading, setLoading] = useState(false);
@@ -9,27 +14,28 @@ function App() {
   const [score, setScore] = useState(0);
   const [gameOver, setGameOver] = useState(true);
 
+  console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY));
+
   const startTrivia = async () => {};
 
   const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
 
   const nextQuestion = () => {};
 
-  const TOTAL_QUESTIONS = 10;
   return (
     <div>
       <h1>REACT QUIZ</h1>
       <button onClick={startTrivia}>Start</button>
       <p>Score : </p>
       <p>Loading Questions ...</p>
-      <QuestionCard
+      {/* <QuestionCard
         questionNumber={number + 1}
         totalQuestions={TOTAL_QUESTIONS}
         question={questions[number].question}
         answers={questions[number].answers}
         userAnswer={userAnswers ? userAnswers[number] : undefined}
         callback={checkAnswer}
-      />
+      /> */}
       <button onClick={nextQuestion}>Next Question</button>
     </div>
   );
